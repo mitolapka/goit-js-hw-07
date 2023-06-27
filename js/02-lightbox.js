f9e189eed2989eec961eed617ff4f4878c86dc9f
mitolapka <124922415+mitolapka@users.noreply.github.com>
2023-06-27 09:01:10 +0300

    change-3

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -3,29 +3,18 @@ import { galleryItems } from './gallery-items.js';
 const gallery = document.querySelector('.gallery');
 
 function createGalleryItem(item) {
-  const galleryItem = document.createElement('li');
-  galleryItem.classList.add('gallery__item');
-
-  const link = document.createElement('a');
-  link.classList.add('gallery__link');
-  link.href = item.original;
-
-  const image = document.createElement('img');
-  image.classList.add('gallery__image');
-  image.src = item.preview;
-  image.alt = item.description;
-
-  link.innerHTML = `
-    <img class="gallery__image" src="${item.preview}" alt="${item.description}">
+  const galleryItem = `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+      </a>
+    </li>
   `;
-  galleryItem.appendChild(link);
-
   return galleryItem;
 }
 
-const galleryItemsMarkup = galleryItems.map(createGalleryItem);
-gallery.append(...galleryItemsMarkup);
-
+const galleryItemsMarkup = galleryItems.map(createGalleryItem).join('');
+gallery.innerHTML = galleryItemsMarkup;
 
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
